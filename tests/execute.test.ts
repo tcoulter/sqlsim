5651ecef1a2fca29bcf40fe40714fdc515e9d813
Tim Coulter <tim@timothyjcoulter.com>
2023-09-10 08:04:51 -0700

    Add a multiline, multi value test

--- tests/execute.test.ts
+++ tests/execute.test.ts
@@ -48,4 +48,32 @@ describe("execute()", () => {
     expect(database.tables['t'].rows[1].cells[0].get()).toBe(21);
     expect(database.tables['t'].rows[2].cells[0].get()).toBe(22);
   })
+
+  test('CREATE and INSERT multiple columns, multiple values', () => {
+    execute(`
+      CREATE TABLE l (
+        name VARCHAR(20),
+        age INT
+      );
+
+      INSERT INTO l VALUES ('Tim', 30), ('Liz', 21);
+    `, storage);
+
+    let database = storage.databases['default'];
+
+    expect(database.hasTable('l')).toBe(true);
+    expect(database.tables['l'].columns.length).toBe(2);
+    expect(database.tables['l'].columns[0]).toBe('name');
+    expect(database.tables['l'].columns[1]).toBe('age');
+
+    expect(database.tables['l'].rows.length).toBe(2);
+
+    // First row
+    expect(database.tables['l'].rows[0].cells[0].get()).toBe('Tim');
+    expect(database.tables['l'].rows[0].cells[1].get()).toBe(30);
+
+    // Second row
+    expect(database.tables['l'].rows[1].cells[0].get()).toBe('Liz');
+    expect(database.tables['l'].rows[1].cells[1].get()).toBe(21);
+  })
 })
