e95b09d5ac5b9b89ffd50ca87448216c53b5c3c6
Tim Coulter <tim@timothyjcoulter.com>
2023-09-20 08:50:10 -0700

    Whoops, it would help if there wasn't a syntax error

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import Storage from "./storage";
 import execute from "./execute";
 
 let SQLSim = {
-  run: (sql:string, storage?:Storage) {
+  run: function (sql:string, storage?:Storage) {
     storage = storage || new Storage();
 
     return execute(sql, storage);
